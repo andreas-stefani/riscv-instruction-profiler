a3db6ad7d2b185a09c1d55dd21776a06b592868a
andreas-stefani <andreas.e.stefani@gmail.com>
2026-03-17 23:52:36 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,35 +1,27 @@
 #!/bin/bash
 
 declare -A sources=(
-    [fib]="programs/fibonacci.c"
-    [cpy]="programs/array_copy.c"
-    [primef]="programs/prime_factor.c"
-    [qsort]="programs/quicksort.c"
-    [sieve]="programs/sieve.c"
+    [fib]="fibonacci.c"
+    [cpy]="array_copy.c"
+    [primef]="prime_factor.c"
+    [qsort]="quicksort.c"
+    [sieve]="sieve.c"
 )
+
 BASE_DIR=$(pwd)
 export PATH=$PATH:$BASE_DIR/riscv/bin
+
 rm -rf output
-mkdir -p output/bins
-mkdir -p output/traces
+mkdir -p output
 
 for name in "${!sources[@]}"; do
-    riscv64-unknown-elf-gcc -static -Wall ${sources[$name]} -lm -o  output/bins/${name}_riscv
+    riscv64-unknown-elf-gcc -static -Wall programs/${sources[$name]} -lm -o  output/${name}_riscv
 done
-
-echo -e "\nCompilation done. Saved in : output/bins/\n"
+echo -e "\nCompilation done. Saved in : output/\n"
 
 for name in "${!sources[@]}"; do
-    echo -e "\nTracing $name...\n"
-    qemu-riscv64 -d in_asm ./output/bins/${name}_riscv 2> output/traces/${name}_trace.txt
+    riscv64-unknown-elf-objdump -d output/${name}_riscv > output/${name}.dump
 done
-
-echo -e "\nDone. Saved in : output/traces/\n"
-
-for name in "${!sources[@]}"; do
-    riscv64-unknown-elf-objdump -d output/bins/${name}_riscv > output/${name}.dump
-done
-
 echo -e "\nDumps done. Saved in : output/\n"
 
 python programs/analyse.py
